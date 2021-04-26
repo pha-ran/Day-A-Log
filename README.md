c1640ec5470235b45f9e33048daba0cd70dcf48a
pha-ran <pha-ran@naver.com>
2021-04-27 02:38:42 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Day A Log
---
+++ app/src/main/java/com/day_a_log/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.day_a_log
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
