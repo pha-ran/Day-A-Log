72ad7197c977a27eec044703fa4e019b02cc1e96
pha-ran <pha-ran@naver.com>
2021-06-23 21:55:09 +0900

    Add Login Fragment

--- app/src/main/java/com/day_a_log/LoginFragment.kt
+++ app/src/main/java/com/day_a_log/LoginFragment.kt
@@ -5,16 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.day_a_log.databinding.FragmentHomeBinding
+import com.day_a_log.databinding.FragmentLoginBinding
 
-class HomeFragment : Fragment() {
-    private lateinit var binding : FragmentHomeBinding
+class LoginFragment : Fragment() {
+    private lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+            savedInstanceState: Bundle?,
     ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container,false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         return binding.root
     }
