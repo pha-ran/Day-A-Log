1332018c710e63d56820da107eca9cc389608d0f
pha-ran <pha-ran@naver.com>
2021-06-25 03:55:04 +0900

    Add Find Fragment

---
+++ app/src/main/java/com/day_a_log/FindFragment.kt
@@ -0,0 +1,21 @@
+package com.day_a_log
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.day_a_log.databinding.FragmentFindBinding
+
+class FindFragment : Fragment() {
+    private lateinit var binding : FragmentFindBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentFindBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
