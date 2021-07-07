152e275103f5f57f194f0a3f840deb1a6f9be007
pha-ran <pha-ran@naver.com>
2021-07-07 22:42:57 +0900

    Add ApplicationClass.kt, XAccessTokenInterceptor.kt

---
+++ app/src/main/java/com/day_a_log/config/XAccessTokenInterceptor.kt
@@ -0,0 +1,21 @@
+package com.day_a_log.config
+
+import com.day_a_log.config.ApplicationClass.Companion.X_ACCESS_TOKEN
+import com.day_a_log.config.ApplicationClass.Companion.sSharedPreferences
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+
+class XAccessTokenInterceptor : Interceptor {
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder: Request.Builder = chain.request().newBuilder()
+        val jwtToken: String? = sSharedPreferences.getString(X_ACCESS_TOKEN, null)
+        if (jwtToken != null) {
+            builder.addHeader("X-ACCESS-TOKEN", jwtToken)
+        }
+        return chain.proceed(builder.build())
+    }
+}
