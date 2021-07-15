771b7dc0e2e4aca2b921b90371f6327f29168f58
pha-ran <pha-ran@naver.com>
2021-07-15 20:57:34 +0900

    Modify SplashActivity.kt, LoginActivity.kt, activity_login.xml, fragment_login.xml

--- app/src/main/java/com/day_a_log/src/login/LoginActivity.kt
+++ app/src/main/java/com/day_a_log/src/login/LoginActivity.kt
@@ -1,14 +1,16 @@
 package com.day_a_log.src.login
 
 import android.os.Bundle
+import com.day_a_log.R
 import com.day_a_log.config.BaseActivity
 import com.day_a_log.databinding.ActivityLoginBinding
+import com.day_a_log.src.login.login.LoginFragment
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        supportFragmentManager.beginTransaction().replace(R.id.fl, LoginFragment()).commitAllowingStateLoss()
     }
 }
