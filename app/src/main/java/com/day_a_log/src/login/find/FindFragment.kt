bb6d7810142792e9e8599151ff9ff48966ba4003
pha-ran <pha-ran@naver.com>
2021-07-25 23:46:00 +0900

    Modify FindFragment.kt, fragment_find.xml

--- app/src/main/java/com/day_a_log/src/login/find/FindFragment.kt
+++ app/src/main/java/com/day_a_log/src/login/find/FindFragment.kt
@@ -5,12 +5,21 @@ import android.view.View
 import com.day_a_log.R
 import com.day_a_log.config.BaseFragment
 import com.day_a_log.databinding.FragmentFindBinding
+import com.day_a_log.src.login.LoginActivity
+import com.day_a_log.src.login.login.LoginFragment
 
 class FindFragment : BaseFragment<FragmentFindBinding>(FragmentFindBinding::bind, R.layout.fragment_find) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        
+        binding.ivExit.setOnClickListener {
+            (activity as LoginActivity).replaceFragment(LoginFragment())
+        }
+
+        binding.btnSend.setOnClickListener {
+            binding.linearKey.visibility = View.VISIBLE
+            binding.btnSend.visibility = View.GONE
+        }
     }
 }
