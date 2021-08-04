4ca8a44c27f122ce86aaca4bdc35973b6a163544
pha-ran <pha-ran@naver.com>
2021-08-04 12:07:53 +0900

    Add Toolbar in AddActivity

--- app/src/main/java/com/day_a_log/src/add/AddActivity.kt
+++ app/src/main/java/com/day_a_log/src/add/AddActivity.kt
@@ -1,6 +1,9 @@
 package com.day_a_log.src.add
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.day_a_log.R
 import com.day_a_log.config.BaseActivity
 import com.day_a_log.databinding.ActivityAddBinding
 
@@ -10,5 +13,27 @@ class AddActivity : BaseActivity<ActivityAddBinding>(ActivityAddBinding::inflate
         super.onCreate(savedInstanceState)
 
         showCustomToast("test")
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_activity_add_back)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.tb_add_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+            R.id.tb_add_next -> {
+                showCustomToast("next")
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
