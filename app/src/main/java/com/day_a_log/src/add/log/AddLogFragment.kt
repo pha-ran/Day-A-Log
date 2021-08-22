9b4e9e11026ce9fcd0a283ad141690d2743ff19a
pha-ran <pha-ran@naver.com>
2021-08-22 17:56:43 +0900

    Add AddLogFragment

---
+++ app/src/main/java/com/day_a_log/src/add/log/AddLogFragment.kt
@@ -0,0 +1,16 @@
+package com.day_a_log.src.add.log
+
+import android.os.Bundle
+import android.view.View
+import com.day_a_log.R
+import com.day_a_log.config.BaseFragment
+import com.day_a_log.databinding.FragmentAddLogBinding
+
+class AddLogFragment : BaseFragment<FragmentAddLogBinding>(FragmentAddLogBinding::bind, R.layout.fragment_add_log) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        
+    }
+}
