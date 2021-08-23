da0c42b08bd282c05938820fcd8ddeeb2e0fe6e4
pha-ran <pha-ran@naver.com>
2021-08-23 14:29:40 +0900

    Add AddRoutineFragment

---
+++ app/src/main/java/com/day_a_log/src/add/routine/AddRoutineFragment.kt
@@ -0,0 +1,16 @@
+package com.day_a_log.src.add.routine
+
+import android.os.Bundle
+import android.view.View
+import com.day_a_log.R
+import com.day_a_log.config.BaseFragment
+import com.day_a_log.databinding.FragmentAddRoutineBinding
+
+class AddRoutineFragment : BaseFragment<FragmentAddRoutineBinding>(FragmentAddRoutineBinding::bind, R.layout.fragment_add_routine) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
